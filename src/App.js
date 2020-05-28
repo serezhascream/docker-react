8b3e89f59e1bcd8ea19f917d0f587932ee37b689
Sergey Matushenko <matushenko@ucoz-team.net>
2020-05-28 22:08:17 +0300

    edit App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, Motherfucker!
+          Learn React or DIE!
         </a>
       </header>
     </div>
